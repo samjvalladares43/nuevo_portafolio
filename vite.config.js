1bf2ef106a696c92010db2345a7e20378a944f57
samjvalladares43 <samjvalladares43@unicah.edu>
2026-07-06 22:32:27 -0600

    Configurar purge css

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from "vite";
-import HtmlCssPurgePlugin from "vite-plugin-purgecss";
+import { PurgeCSS } from "purgecss";
+
+function purgeCssFromHtml() {
+  return {
+    name: "purge-css-from-html",
+    apply: "build",
+
+    async generateBundle(_, bundle) {
+      const content = Object.values(bundle)
+        .filter((file) => file.type === "asset" && file.fileName.endsWith(".html"))
+        .map((file) => ({
+          raw: String(file.source),
+          extension: "html"
+        }));
+
+      for (const file of Object.values(bundle)) {
+        if (file.type === "asset" && file.fileName.endsWith(".css")) {
+          const result = await new PurgeCSS().purge({
+            content,
+            css: [
+              {
+                raw: String(file.source)
+              }
+            ]
+          });
+
+          file.source = result[0].css;
+        }
+      }
+    }
+  };
+}
 
 export default defineConfig({
   base: "/nuevo_portafolio/",
   plugins: [
-    HtmlCssPurgePlugin()
+    purgeCssFromHtml()
   ]
 });
